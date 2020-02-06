4529755044900ca51c7c4096f2bb84bb38e4f65f
apatinoc4 <apatinoc4@gmail.com>
2020-02-06 16:00:36 -0500

    added files and 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# returns the number of subscribers for a given subreddit
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Returns the number of subscribers of a `subreddit`'''
+
+    ep = 'https://reddit.com/r/{}.json'.format(subreddit)
+    headers = {'User-Agent': 'x'}
+
+    req = requests.get(ep, headers=headers).json()
+    try:
+        data = req.get('data').get('children')[0].get('data')
+    except AttributeError:
+        return 0
+    n = data.get('subreddit_subscribers')
+    return n
