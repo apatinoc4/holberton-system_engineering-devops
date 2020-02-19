4c933b36a3e257a5f0a455bfc568cc7201e0546d
apatinoc4 <apatinoc4@gmail.com>
2020-02-19 13:15:54 -0500

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fix the config file for Apache to direct to the correct wp config file
-exec { 'fix config file':
+# Replaces a typo (.phpp) in the Apache config file
+exec { 'fix typo':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
